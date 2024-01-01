3c3cf5be1009cc3b82b976eed32487ec0bb243e6
Zvieitez <zvieitez@hotmail.com>
2024-01-01 16:37:39 -0300

    projeto celular Zandra Vieitez

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿using DesafioPOO.Models;
+﻿using System.Globalization;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine("Smartphone modelo: Nokia");
+        Smartphone nokia = new Nokia("81 98161-0209","Nokia Tijolão", "000000", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+        Console.WriteLine("*************************************************************************************************************");
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone modelo: Iphone");
+        Smartphone iphone = new Iphone("81 99248-0209", "Iphone 50", "111111", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+        Console.WriteLine("\n");
+    }
+}  
+    
